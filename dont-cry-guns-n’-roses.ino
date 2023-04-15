c2fd95154b0edb47026766fded8044ae69e1b85f
Kudrin German <76187153+melhhior@users.noreply.github.com>
2023-04-15 18:18:52 +0200

    Update dont-cry-guns-n’-roses.ino

--- dont-cry-guns-n’-roses.ino
+++ dont-cry-guns-n’-roses.ino
@@ -18,19 +18,6 @@ int dontcrynotes[] = {
   };
 
 void setup() {
-  int dontcrynotes[] = {
-    NOTE_A3, NOTE_E4, NOTE_A4, NOTE_E5, NOTE_C5, NOTE_A4, NOTE_E4,
-    NOTE_D4, NOTE_A4, NOTE_D5, NOTE_F5, NOTE_D5, NOTE_A4, NOTE_D4,
-    NOTE_G3, NOTE_B3, NOTE_D4, NOTE_G4,  NOTE_G5, NOTE_D5, NOTE_G4, NOTE_D4,
-    NOTE_C4, NOTE_E4, NOTE_G4,
-    NOTE_B3, NOTE_D4, NOTE_G4,
-    NOTE_C4, NOTE_B3,
-  };
-
-  int dontcrynotesduration[] = {
-    10, 5, 5, 5, 5, 5, 5, 10, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 0
-  };
-
   for (int thisNote = 0; dontcrynotes[thisNote]!=-1; thisNote++) {
   if (thisNote == 31)
   {
